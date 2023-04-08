7449e3e1bbec93e3afe225afe8aff67cbc387e8e
yashcshah30 <yashcshah30@gmail.com>
2023-04-08 19:44:31 -0400

    Added landByState data function

--- Data/land.js
+++ Data/land.js
@@ -1,31 +1,35 @@
 import { getClient } from "../config/connection.js";
-const getLand = async (id)=>{
-
-};
-const getAllLand = async ()=>{
-    const client = getClient();
-    const result = await client.collection("land").find().toArray();
-    return result;
-};
-
-const updateLand = async (object) =>{
-
+import { exists, checkInputType } from "../Utils/helpers.js";
+const getLand = async (id) => {};
+const getAllLand = async () => {
+  const client = getClient();
+  const result = await client.collection("land").find().toArray();
+  return result;
 };
 
-const postLand = async (object) =>{
+const updateLand = async (object) => {};
 
-};
+const postLand = async (object) => {};
 
-const removeLand = async(id)=> {
+const removeLand = async (id) => {};
 
+const getLandByState = async (state) => {
+  const client = getClient();
+  const result = await client
+    .collection("land")
+    .find({ state: state })
+    .toArray();
+  return result;
 };
 
 const landData = {
-    getAllLand: getAllLand,
-    getLand:getLand,
-    updateLand: updateLand,
-    postLand: postLand,
-    removeLand:removeLand
-}
+  getAllLand: getAllLand,
+  getLand: getLand,
+  updateLand,
+  updateLand,
+  postLand: postLand,
+  removeLand: removeLand,
+  getLandByState: getLandByState,
+};
 
 export default landData;
--- Routes/index.js
+++ Routes/index.js
@@ -2,6 +2,9 @@ import landRoutes from "./land.js";
 
 const constructorMethod = (app) => {
   app.use("/land", landRoutes);
+  app.use("*", (req, res) => {
+    res.status(400).json({ error: "Not Found" });
+  });
 };
 
 export default constructorMethod;
