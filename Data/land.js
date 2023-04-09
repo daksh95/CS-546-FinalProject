bfac2138d8d3bfa4cbe0bb6d13cd8a21ea2d5d6b
yashcshah30 <yashcshah30@gmail.com>
2023-04-09 19:06:06 -0400

    Validated getLandByState and completed getLandID

--- Data/land.js
+++ Data/land.js
@@ -1,6 +1,23 @@
+import { ObjectId } from "mongodb";
 import { getClient } from "../config/connection.js";
-import { exists, checkInputType } from "../Utils/helpers.js";
-const getLand = async (id) => {};
+import { exists, checkInputType, validStateCodes, arrayLength } from "../Utils/helpers.js";
+
+const getLand = async (id) => {
+  if (!exists(id)) throw new Error("ID parameter does not exists");
+  if (!checkInputType(id, "string"))
+    throw new Error("ID must be of string only");
+  if (id.trim().length === 0) throw new Error("ID cannot be of empty spaces");
+  id = id.trim();
+  if (!ObjectId.isValid(id)) throw new Error("Invalid Object ID");
+  const client = getClient();
+  const result = await client
+    .collection("land")
+    .findOne({ _id: new ObjectId(id) });
+  if (result === null) throw new Error("No Land with such ID");
+  result._id = result._id.toString();
+  return result;
+};
+
 const getAllLand = async () => {
   const client = getClient();
   const result = await client.collection("land").find().toArray();
@@ -14,6 +31,16 @@ const postLand = async (object) => {};
 const removeLand = async (id) => {};
 
 const getLandByState = async (state) => {
+  if (!exists(state)) throw new Error("State parameter does not exists");
+  if (!checkInputType(state, "string"))
+    throw new Error("State parameter must be of type string");
+  if (state.trim().length === 0)
+    throw new Error("State cannot contain empty spaces only");
+  state = state.trim();
+  if (!validStateCodes.includes(state.toUpperCase()))
+    throw new Error(
+      "State parameter must be a valid statecode in abbreviations only"
+    );
   const client = getClient();
   const result = await client
     .collection("land")
--- Utils/helpers.js
+++ Utils/helpers.js
@@ -1,8 +1,8 @@
 // You can add and export any helper functions you want here - if you aren't using any, then you can just leave this file as is
 export let exists = (param) => {
-    if (!param && !(param==false)) return false
-    else return true
-}
+  if (!param && !(param == false)) return false;
+  else return true;
+};
 
 export let stringPalindromes = (str) => {
   let stringArray = str.split("");
@@ -91,3 +91,56 @@ export let emptyArrayCheck = (...args) => {
   }
   return result;
 };
+
+export const validStateCodes = [
+  "AL",
+  "AK",
+  "AZ",
+  "AR",
+  "CA",
+  "CO",
+  "CT",
+  "DE",
+  "FL",
+  "GA",
+  "HI",
+  "ID",
+  "IL",
+  "IN",
+  "IA",
+  "KS",
+  "KY",
+  "LA",
+  "ME",
+  "MD",
+  "MA",
+  "MI",
+  "MN",
+  "MS",
+  "MO",
+  "MT",
+  "NE",
+  "NV",
+  "NH",
+  "NJ",
+  "NM",
+  "NY",
+  "NC",
+  "ND",
+  "OH",
+  "OK",
+  "OR",
+  "PA",
+  "RI",
+  "SC",
+  "SD",
+  "TN",
+  "TX",
+  "UT",
+  "VT",
+  "VA",
+  "WA",
+  "WV",
+  "WI",
+  "WY",
+];
