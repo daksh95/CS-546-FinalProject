5658d6aca7a4a7fa2f8e34c73e3f1d32db9acc11
yashcshah30 <yashcshah30@gmail.com>
2023-04-23 00:13:40 -0400

    started with routes of getLandByState

--- Controllers/land.js
+++ Controllers/land.js
@@ -1,4 +1,5 @@
 import landData from "../Data/land.js";
+import { checkInputType, exists, validStateCodes } from "../Utils/helpers.js";
 
 const getLand = async (req, res) => {
   const result = await landData.getLand(req.params.id);
@@ -7,6 +8,47 @@ const getLand = async (req, res) => {
 const getAllLand = async (req, res) => {
   const result = await landData.getAllLand();
   res.status(200).json(result);
+};
+
+const getLandByState = async (req, res) => {
+  res.status(200).render("getLandByState", { title: "Get Lands By State" });
+};
+
+const postLandByState = async (req, res) => {
+  let state = req.body.stateInput;
+  let error = [];
+  if (!exists(state)) error.push("State is not entered");
+  if (!checkInputType(state, "string"))
+    error.push("State must be of type string only");
+  if (state.trim().length === 0)
+    error.push("State cannot contain empty spaces only");
+  state = state.trim();
+  if (!validStateCodes.includes(state.toUpperCase()))
+    error.push(
+      "State parameter must be a valid statecode in abbreviations only"
+    );
+  if (error.length !== 0)
+    return res.status(400).render("getLandByState", {
+      title: "Get Lands By State",
+      hasError: true,
+      error: error,
+    });
+
+  try {
+    let landByState = await landData.getLandByState(state);
+    res
+      .status(200)
+      .render("displayLandByState", {
+        title: "Lands",
+        landByState: landByState,
+      });
+  } catch (error) {
+    return res.status(400).render("getLandByState", {
+      title: "Get Lands By State",
+      hasError: true,
+      error: error.message,
+    });
+  }
 };
 
 const updateLand = async (req, res) => {};
@@ -23,4 +65,12 @@ const removeLand = async (id) => {};
 //     removeLand:removeLand
 // }
 
-export { getAllLand, getLand, updateLand, postLand, removeLand };
+export {
+  getAllLand,
+  getLand,
+  updateLand,
+  postLand,
+  removeLand,
+  getLandByState,
+  postLandByState,
+};
--- Routes/land.js
+++ Routes/land.js
@@ -1,8 +1,16 @@
 import express from "express";
 const routes = express.Router();
-import {getAllLand, getLand, updateLand, postLand, removeLand, } from "../Controllers/land.js";
-routes.route('/').get(getAllLand).post(postLand);
-routes.route('/:id').get(getLand).patch(updateLand).delete(removeLand);
+import {
+  getAllLand,
+  getLand,
+  updateLand,
+  postLand,
+  removeLand,
+  getLandByState,
+  postLandByState
+} from "../Controllers/land.js";
 
+routes.route("/").get(getLandByState).post(postLandByState);
+routes.route("/:id").get(getLand).patch(updateLand).delete(removeLand);
 
 export default routes;
